07fc1a477da6db9a626fc33a2eb453a5efd7d761
tmfgml208 <tmfgml208@naver.com>
2023-11-05 22:02:48 +0900

    login/register/firebase

---
+++ src/Components/LoginSignup/firebase_config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDgsb0kVsGI4XxHMLpaim_-SGmIVIzM0fY",
+  authDomain: "sppj-1ce61.firebaseapp.com",
+  projectId: "sppj-1ce61",
+  storageBucket: "sppj-1ce61.appspot.com",
+  messagingSenderId: "182384163402",
+  appId: "1:182384163402:web:ef15f21ca3e6e5bbf984b3",
+  measurementId: "G-B7W5NKM36H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
