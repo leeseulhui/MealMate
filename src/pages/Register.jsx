2e85394e3b7f82055ae0ce76fd226c4a0359f334
seulhui <tmfgml208@naver.com>
2023-11-08 19:43:15 +0900

    refeat:auth

---
+++ src/pages/Register.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import Add from "../img/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
+
+const Register = () => {
+  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      //사용자 등록
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //프로필 업데이트
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //firestore에 사용자 생성
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
+        });
+      });
+    } catch (err) {
+      setErr(true);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">앱 이름 넣을거임 </span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="id" placeholder="student_id" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>이미지</span>
+          </label>
+          <button disabled={loading}>Sign up</button>
+          {loading && "잠시만 기다려주세요..."}
+          {err && <span>다시 시도해주십시오.</span>}
+        </form>
+        <p>
+          You do have an account? <Link to="/register">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
